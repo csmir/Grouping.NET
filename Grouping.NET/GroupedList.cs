82748754e4e2a682e56420d1b40325df05edf12a
Armano den Boef <68127614+Rozen4334@users.noreply.github.com>
2025-05-08 22:10:54 +0200

    'The lack of => pains me'

--- Grouping.NET/GroupedList.cs
+++ Grouping.NET/GroupedList.cs
@@ -3,7 +3,7 @@
 namespace Grouping;
 
 /// <summary>
-///     A <see cref="IList{T}"/> collection that is grouped by a key.
+///     A <see cref="List{T}"/> that is grouped by a key.
 /// </summary>
 /// <typeparam name="TKey">The key type of this grouped list.</typeparam>
 /// <typeparam name="T">The element type of this grouped list.</typeparam>
@@ -18,20 +18,14 @@ public class GroupedList<TKey, T> : List<T>, IGrouping<TKey, T>, ICollection<T>,
     /// </summary>
     /// <param name="key">The object to consider the key of this grouped list.</param>
     public GroupedList(TKey key, int capacity)
-        : base(capacity)
-    {
-        Key = key;
-    }
+        : base(capacity) => Key = key;
 
     /// <summary>
     ///     Creates a new instance of <see cref="GroupedList{TKey, TElement}"/> with the specified grouping.
     /// </summary>
     /// <param name="enumerable">The grouping to consider the key and elements of this grouped list.</param>
     public GroupedList(IGrouping<TKey, T> enumerable)
-        : base(enumerable)
-    {
-        Key = enumerable.Key;
-    }
+        : base(enumerable) => Key = enumerable.Key;
 
     /// <summary>
     ///     Creates a new instance of <see cref="GroupedList{TKey, TElement}"/> with the specified key and enumerable.
@@ -39,8 +33,5 @@ public class GroupedList<TKey, T> : List<T>, IGrouping<TKey, T>, ICollection<T>,
     /// <param name="key">The object to consider the key of this grouped list.</param>
     /// <param name="enumerable">The values to consider the elements of this grouped list.</param>
     public GroupedList(TKey key, IEnumerable<T> enumerable)
-        : base(enumerable)
-    {
-        Key = key;
-    }
+        : base(enumerable) => Key = key;
 }
